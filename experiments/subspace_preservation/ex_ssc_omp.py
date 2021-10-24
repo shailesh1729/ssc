24cfeab391fb1df89898c9ce55eaac568473ec5b
Shailesh Kumar <shailesh@indigits.com>
2021-10-24 18:07:06 +0530

    use batch_build_representation_omp

--- experiments/subspace_preservation/ex_ssc_omp.py
+++ experiments/subspace_preservation/ex_ssc_omp.py
@@ -8,5 +8,6 @@ from ssc.subspace_preservation import bench_subspace_preservation
 import cr.sparse.cluster.ssc as ssc
 
 key = random.PRNGKey(0)
-bench_subspace_preservation(key, ssc.build_representation_omp_jit, 
+solver = lambda X, K: ssc.batch_build_representation_omp_jit(X, K, 800)
+bench_subspace_preservation(key, solver, 
     'ssc-omp-subspace-preservation-random-subspaces')
